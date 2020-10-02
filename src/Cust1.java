d451befc4b3287c2eab913d686100411dbb28c18
sujata swami <sujataswami12@gmail.com>
2020-10-02 12:22:03 +0530

    first commit

---
+++ src/Cust1.java
@@ -0,0 +1,13 @@
+public class Cust1 {
+    int custId;
+    String name;
+
+    public static void main(String[] args) {
+        Cust1 c=new Cust1();
+        c.custId=121;
+        c.name="abc";
+    }
+
+
+
+}
